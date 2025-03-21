9b8048a914f67fd99759cbb41cd9e0a2362f7f96
aizirekkanybekkyzy <kanybekkyzyaizirek11@gmail.com>
2025-03-21 21:17:01 +0600

    updated project

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const User = () => {
     return (
       <div>
-                <h3 className='text-slate-50'>Trello Clone</h3>
-            <div className="right flex items-center space-x-4">
-                <span>Remote dev</span>
+                <h3 >Trello Clone</h3>
+            <div >
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, hic!</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laborum nostrum cupiditate atque mollitia corrupti.</p>
             </div>
         </div>
     );
