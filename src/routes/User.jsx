2e68b1e89b2dc11db8cadad548402847680aa8ad
aizirekkanybekkyzy <kanybekkyzyaizirek11@gmail.com>
2025-03-22 20:54:02 +0600

    updated project

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -6,6 +6,7 @@ const User = () => {
                 <h3 >Trello Clone</h3>
             <div >
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, hic!</span>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores sequi quisquam nulla omnis nostrum aliquid error veritatis voluptatem doloribus.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laborum nostrum cupiditate atque mollitia corrupti.</p>
             </div>
         </div>
