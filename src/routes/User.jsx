b4856d4d3c67f4a5e57f5d8a35d45aaa2cb5afc9
aizirekkanybekkyzy <kanybekkyzyaizirek11@gmail.com>
2025-03-22 22:11:57 +0600

    added new text in user.jsx

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -9,6 +9,7 @@ const User = () => {
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores sequi quisquam nulla omnis nostrum aliquid error veritatis voluptatem doloribus.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laborum nostrum cupiditate atque mollitia corrupti.</p>
                 <h2>Lorem ipsum dolor sit amet.</h2>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eaque iusto totam?</h4>
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, delectus!</h3>
                 <button>click</button>
             </div>
