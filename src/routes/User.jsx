9b22355f7e5ed7c3f338677cbe9b700ab01e54ac
aizirekkanybekkyzy <kanybekkyzyaizirek11@gmail.com>
2025-03-22 22:01:52 +0600

    new in project

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -9,6 +9,7 @@ const User = () => {
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores sequi quisquam nulla omnis nostrum aliquid error veritatis voluptatem doloribus.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laborum nostrum cupiditate atque mollitia corrupti.</p>
                 <h2>Lorem ipsum dolor sit amet.</h2>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, delectus!</h3>
                 <button>click</button>
             </div>
         </div>
